a05dd0c1dd37ace44ce5a1884d96598e800f9019
Andrew Ballinger <aballinger@payjunction.com>
2016-08-22 10:22:07 -0700

    One shot, one point

---
+++ app/make-editable-app.js
@@ -0,0 +1,25 @@
+/*global angular $*/
+angular.module('editApp', ['ngAnimate'])
+  .directive('makeEditable', () => {
+    return {
+      restrict: 'E',
+      templateUrl: 'make-editable-template.html',
+      transclude: true,
+      replace: true,
+      scope: true,
+      link : (scope, element, attrs) => {
+        var text = $(element).find('.editable-region');
+
+        scope.editable = false;
+        scope.toggleEditability = () => {
+          scope.editable = !scope.editable
+          if (scope.editable) {
+            text.attr('contenteditable','');
+          } else {
+            text.removeAttr('contenteditable');
+          }
+          return;
+        }
+      }
+    }  
+  });
